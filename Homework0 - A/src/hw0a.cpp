96c1e0c72c353b848da12f89b6b02fbe1be35661
Jacob Barnett <jacob.barnett0417@gmail.com>
2020-09-02 20:02:36 -0400

    Added files

---
+++ Homework0 - A/src/hw0a.cpp
@@ -0,0 +1,13 @@
+// Jacob Barnett
+// EECS 2510 Non-Linear Data Structures
+// Homework 0 - Part A
+
+#include <iostream>
+using namespace std;
+
+int main(int argCount, char *argValues[]) {
+	cout << "Jacob Barnett" << endl;
+	cout << "Eclipse" << endl;
+	cout << "Yellow" << endl;
+	return EXIT_SUCCESS;
+}
